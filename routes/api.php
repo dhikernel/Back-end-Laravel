cfc5d29c3fad0024de7e05a0462ab381a19068c3
Diego Pereira <san_diego19@hotmail.com>
2022-02-06 20:28:24 -0300

    Rotas protegidas com JWT Token

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,19 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
+Route::group([
+    'middleware' => ['jwt.verify'],
+    'prefix' => 'v1'
+], function ($router) {
+
+Route::post('login', [\App\Http\Controllers\Api\AuthController::class, 'login'])->name('login');
+
+Route::post('logout', [\App\Http\Controllers\Api\AuthController::class, 'logout'])->name('logout');
+
+Route::post('refresh', [\App\Http\Controllers\Api\AuthController::class, 'refresh'])->name('refresh');
+
+Route::post('me', [\App\Http\Controllers\Api\AuthController::class, 'me'])->name('me');
+
 Route::apiResource('/users/{user}/vehicles', VehicleController::class);
 
 Route::put('/user/{user}', [UserController::class, 'update']);
@@ -19,7 +32,7 @@ Route::delete('/user/{id}', [UserController::class, 'destroy']);
 Route::get('/user/{id}', [UserController::class, 'show']);
 Route::post('/user', [UserController::class, 'store']);
 Route::get('/user', [UserController::class, 'index']);
-
+});
 Route::get('/', function(){
 return response()->json(['message' => 'ok!']);
 });
