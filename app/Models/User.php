21061b872ff72728f37e989c0292986c99adac74
Diego Pereira <san_diego19@hotmail.com>
2022-02-06 20:26:54 -0300

    Implementaçao JWT Token

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,60 @@
 
 namespace App\Models;
 
+use App\Http\Middleware\Authenticate;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory;
+    use Notifiable;
 
-    protected $fillable = ['name', 'email', 'password', 'cpf', 'rg', 'date_of_birth', 'zip_code', 'address', 'number', 'city', 'state'];
+    protected $table = 'users';
+
+    protected $primarykey = 'id';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'cpf',
+        'rg',
+        'date_of_birth',
+        'zip_code',
+        'address',
+        'number',
+        'city',
+        'state'
+    ];
+
+    // Rest omitted for brevity
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [
+            'id' => $this->id,
+            'email' => $this->email
+        ];
+    }
 
     public function vehicles()
     {
